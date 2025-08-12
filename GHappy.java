e1b1499ee5cc3c3d67cfee2aa0685ca36fbe3542
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2025-08-13 01:08:37 +0200

    Bug detected and corrected GHappy.java

---
+++ GHappy.java
@@ -0,0 +1,22 @@
+// Code is collected from github.com/SERG-Delft/mooc-software-testing for learning purpose
+
+package tudelft.ghappy;
+
+public class GHappy {
+
+    public boolean gHappy(String str) {
+        assert str!=null;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'g') {
+                // Bug Detected
+                // if (i >= 0 && str.charAt(i-1) == 'g') { continue; } // i>=0 and i-1 indicate index -1, created error
+                if (i >0 && str.charAt(i-1) == 'g') { continue; }
+                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
